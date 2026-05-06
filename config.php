0cfc5e7af066ebd44dc1447ba62d6aee99d9e0d0
leraaranza1400-web <leraaranza1400@gmail.com>
2026-05-06 01:29:14 -0600

    fix: corregir syntax error en config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $password = '456789';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PD O::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
